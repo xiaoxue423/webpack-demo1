6ff25c0182f30668228433ea4148c86e8064f8c5
xiaoxue0423 <xiaoxuexue19980423@163.com>
2021-01-15 14:36:08 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   // mode是用来切换模式的
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
